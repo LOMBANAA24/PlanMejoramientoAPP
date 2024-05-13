2315d4f54ca018b4b4c473c5bf259e974ee7e2e6
Cristian Lombana <cridlombana1@soy.sena.edu.co>
2024-05-12 21:04:18 -0500

    Realicé cambios en mis archivo

--- src/config/config.js
+++ src/config/config.js
@@ -1,17 +1,19 @@
 const mysql = require('mysql2');
 const db = mysql.createConnection({
- host: '192.168.36.203', // Dirección de tu base de datos
- port: '8081', // Puerto de tu base de datos
- user: 'root', 
- password: '24023', 
- database: 'motoscev6',
- authPlugins : {
-    mysql_clear_password: () => Buffer.from('24023','utf-8' ),
-    caching_sha2_password : true,
- }
+    host: process.env.DB_HOST, // Utilizando la variable de entorno
+    port: process.env.DB_PORT, 
+    user: process.env.DB_USER, 
+    password: process.env.DB_PASSWORD, 
+    database: process.env.DB_NAME, 
+    authPlugins: {
+        mysql_clear_password: () => Buffer.from(process.env.DB_PASSWORD, 'utf-8'),
+        caching_sha2_password: true,
+    }
 });
+
 db.connect(function(err) {
- if (err) throw err; 
- console.log('Base de datos conectada');
+    if (err) throw err; 
+    console.log('Base de datos conectada');
 });
+
 module.exports = db;
